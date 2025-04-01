d55cad04bd5b68b45b9a0efad6a50669745e8616
unknown <martin.majer@infis.cz>
2025-04-01 09:36:44 +0200

    text

---
+++ main.py
@@ -0,0 +1,9 @@
+předměty = ["Čeština","Dějepis","Matika","Biologie","Fyzika","Chemie"]
+x = předměty[0]
+x = len(předměty)
+předměty.remove("Dějepis")
+předměty.sort
+předměty.reverse
+předměty.append
+for x in předměty:
+    print(x)
